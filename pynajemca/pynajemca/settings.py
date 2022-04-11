7eae1440859c0c9698267014d0ec0a86355d4fee
IT-Buster <bartek@bk.waw.pl>
2022-04-11 17:18:35 +0200

    clean env

--- pynajemca/pynajemca/settings.py
+++ pynajemca/pynajemca/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-##SECRET_KEY = 'django-insecure-@2#lnse5t50=3h7#@60ctrtwz)-!_hva)(b5&65xtd)kto=44a'
+
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
